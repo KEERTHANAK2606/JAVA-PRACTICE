12c24771c440d1d7bd7891eb248d81f71a79afe2
KEERTHANA <keerthanavk69@gmail.com>
2026-07-06 21:03:55 +0530

    Update CountDigits.java

--- CountDigits.java
+++ CountDigits.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class CountDigits{
+public class CountDigit{
     public static void main(String[] args){
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
