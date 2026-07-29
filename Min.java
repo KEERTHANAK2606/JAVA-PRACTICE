bd7f8b7a61ebc1c962e24eae1110e0c363292bf4
KEERTHANA <keerthanavk69@gmail.com>
2026-07-29 22:41:59 +0530

    Create Min.java
    To find minimum element in an array

---
+++ Min.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Min {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+        int min = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.print(min);
+    }
+}
